d0d68e45b931efc6ba2466bbd9efb3dce7f18851
Jack Hillman <jack@atomix.com.au>
2021-03-09 17:04:04 +1030

    ✨ Setup test 2

--- 1-node/src/fizz-buzz.js
+++ 1-node/src/fizz-buzz.js
@@ -7,5 +7,5 @@
  * }]
  */
 module.exports = (start, end) => {
-  throw new Error('TODO: Implement FizzBuzz')
+  throw 'TODO: Implement FizzBuzz'
 }
---
+++ 2-react/src/fizzBuzz.ts
@@ -0,0 +1,10 @@
+export type FizzBuzzOutput = 'Fizz' | 'Buzz' | 'FizzBuzz' | ''
+
+export type FizzBuzzResult = {
+  input: number
+  output: FizzBuzzOutput
+}
+
+export const fizzBuzz = (start: number, end: number): FizzBuzzResult[] => {
+  throw 'TODO: Implement FizzBuzz'
+}
